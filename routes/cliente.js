95bc5e0c0b437c1d3c2fcbdf1790a20dab91492f
Henrique Savoia <henriquestefani@hotmail.com>
2019-12-12 01:28:12 -0300

    Novos métodos de cliente

--- routes/cliente.js
+++ routes/cliente.js
@@ -13,7 +13,14 @@ router.get('/selectAll', (req, res, next) => {
 router.post('/login', (req, res, next) => {
     new ClienteService(pool)
     .login(req.body)
-    .then(data => res.status(201).send(data))
+    .then(data => res.status(200).send(data))
+    .catch(next)
+});
+
+router.post('/insert', (req, res, next) => {
+    new ClienteService(pool)
+    .insert(req.body)
+    .then(data => res.status(201).send({status: 'success'}))
     .catch(next)
 });
 
--- services/cliente.js
+++ services/cliente.js
@@ -14,10 +14,23 @@ class ClienteService {
 
     login(loginData) {
         return new Promise((resolve, reject) => {
-            this._connection.query('SELECT * FROM cliente WHERE nome_usuario = ? AND senha=?', [loginData['nome_usuario'],loginData['senha']], (err, cliente) => {
+            this._connection.query('SELECT * FROM cliente WHERE email = ? AND senha=?', [loginData['email'],loginData['senha']], (err, cliente) => {
                 if(err) return reject(err);
                 if(!cliente.length) return reject('Usuário inválido');
                 resolve(cliente[0]);
+            })
+        });
+    }
+
+    insert(cliente) {
+        return new Promise((resolve, reject) => {
+            this._connection.query('SELECT * FROM cliente WHERE email = ?', cliente['email'], (err, data) => {
+                if(err) return reject(err);
+                if(data.length) return reject ('Usuário já existe');
+                this._connection.query('INSERT INTO cliente SET ?', cliente, (err, clienteInserido) => {
+                    if(err) return reject(err);
+                    resolve(clienteInserido);
+                })
             })
         });
     }
